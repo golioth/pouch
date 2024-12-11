42a14bbccaf18340a7d4568a5b5fe707aaf52b42
Sam Friedman <sam@golioth.io>
2024-12-11 17:36:46 -0500

    info: implement info characteristic
    Implement the Info characteristic to serve payloads
    serialized into CBOR and split into Toothfairy packets.
    This uses a hardcoded Device ID for now.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- src/info_characteristic.c
+++ src/info_characteristic.c
@@ -2,14 +2,29 @@
  * Copyright (c) 2024 Golioth
  */
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(info_chrc, LOG_LEVEL_DBG);
+
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
 
 #include "toothfairy_declarations.h"
 #include "toothfairy_uuids.h"
+#include "packetizer.h"
+
+#include <cddl/info_encode.h>
+
+#define INFO_CHRC_MAX_SIZE 64
+#define DEVICE_ID "123456789"
 
 static const struct bt_uuid_128 tf_info_chrc_uuid = BT_UUID_INIT_128(TF_UUID_GOLIOTH_INFO_CHRC_VAL);
+
+static struct info_ctx
+{
+    uint8_t buf[INFO_CHRC_MAX_SIZE];
+    struct tf_packetizer *packetizer;
+} info_chrc_ctx;
 
 static ssize_t info_read(struct bt_conn *conn,
                          const struct bt_gatt_attr *attr,
@@ -17,7 +32,54 @@ static ssize_t info_read(struct bt_conn *conn,
                          uint16_t len,
                          uint16_t offset)
 {
-    return 0;
+    /* Force TF packets into a single MTU */
+
+    if (offset > 0)
+    {
+        return 0;
+    }
+
+    struct info_ctx *ctx = attr->user_data;
+
+    /* Initialize packetizer and payload on first packet */
+
+    if (NULL == ctx->packetizer)
+    {
+        /* Serialize Info CBOR */
+
+        struct tf_info info = {
+            .tf_info_enc_type_choice = tf_info_enc_type_plaintext_tstr_c,
+            .tf_info_device_id =
+                {
+                    .value = DEVICE_ID,
+                    .len = sizeof(DEVICE_ID) - 1,
+                },
+        };
+
+        size_t info_len = 0;
+        cbor_encode_tf_info(ctx->buf, INFO_CHRC_MAX_SIZE, &info, &info_len);
+
+        ctx->packetizer = tf_packetizer_start(ctx->buf, info_len);
+    }
+
+    size_t buf_len = len;
+    enum tf_packetizer_result res = tf_packetizer_get(ctx->packetizer, buf, &buf_len);
+
+    if (TF_PACKETIZER_ERROR == res)
+    {
+        int error = tf_packetizer_error(ctx->packetizer);
+        LOG_ERR("Toothfairy packetizer failed: %d", error);
+        tf_packetizer_finish(ctx->packetizer);
+        ctx->packetizer = NULL;
+        return 0;
+    }
+    if (TF_PACKETIZER_NO_MORE_DATA == res)
+    {
+        tf_packetizer_finish(ctx->packetizer);
+        ctx->packetizer = NULL;
+    }
+
+    return buf_len;
 }
 
 TOOTHFAIRY_CHARACTERISTIC(info,
@@ -26,4 +88,4 @@ TOOTHFAIRY_CHARACTERISTIC(info,
                           BT_GATT_PERM_READ,
                           info_read,
                           NULL,
-                          NULL);
+                          &info_chrc_ctx);
