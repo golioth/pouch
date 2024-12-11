37ae16df7337db8624b8a4c2aa4ceb9e98202429
Sam Friedman <sam@golioth.io>
2024-12-11 00:30:34 -0500

    peripheral: add golioth service
    Add the golioth service to the GATT database. This service
    is currently empty, but as characteristics are built they
    will be automatically added to this service.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- src/peripheral.c
+++ src/peripheral.c
@@ -5,8 +5,10 @@
 #include <stdlib.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
 
+#include "toothfairy_declarations.h"
 #include <toothfairy/peripheral.h>
 
 #include "toothfairy_uuids.h"
@@ -16,9 +18,32 @@ struct toothfairy_peripheral
     void *unused;
 };
 
+static const struct bt_uuid_128 tf_svc_uuid = BT_UUID_INIT_128(TF_UUID_GOLIOTH_SVC_VAL);
+
+TOOTHFAIRY_SERVICE(&tf_svc_uuid);
+
+static struct bt_gatt_service golioth_svc = {
+    .attrs = TOOTHFAIRY_ATTR_ARRAY_PTR,
+};
+
 struct toothfairy_peripheral *toothfairy_peripheral_create(void)
 {
-    return malloc(sizeof(struct toothfairy_peripheral));
+    struct toothfairy_peripheral *tf = malloc(sizeof(struct toothfairy_peripheral));
+    if (NULL == tf)
+    {
+        goto finish;
+    }
+
+    TOOTHFAIRY_ATTR_ARRAY_LEN(&golioth_svc.attr_count);
+    int err = bt_gatt_service_register(&golioth_svc);
+    if (0 != err)
+    {
+        free(tf);
+        tf = NULL;
+    }
+
+finish:
+    return tf;
 }
 
 int toothfairy_peripheral_destroy(struct toothfairy_peripheral *tf_peripheral)
