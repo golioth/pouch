e6ff6582cfd9029b212b4a93ef03c82bc8198d95
Sam Friedman <sam@golioth.io>
2025-04-02 23:15:26 -0400

    downlink: add strawman API and trivial implementation
    Add a strawman transport API for downlink to use for
    transport development. The strawman API has roughly
    the expected shape as the eventual API, but it is
    expected that arguments and return values will change.
    This commit is not binding on future development of this
    API.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

---
+++ include/pouch/transport/downlink.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 Golioth, Inc.
+ */
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+/**
+ * @file downlink.h
+ * @brief Downlink API for Pouch Transport layer implementations
+ */
+
+/** Start a new downlink session */
+void pouch_downlink_start(void);
+
+/** Push downlink data into Pouch */
+void pouch_downlink_push(const void *buf, size_t buf_len);
+
+/** Finish the downlink session */
+void pouch_downlink_finish(void);
