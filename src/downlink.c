5b2e7975538f1004762240a35497b41eb877174b
Marcin Niestroj <m.niestroj@emb.dev>
2025-05-18 07:28:10 +0200

    downlink: implement block reassembly and entry parsing
    There are 2 queues that let buffer downlink consumption:
     * decrypt - all received transport data is reassemblied to blocks and
       those blocks are enqueued for decryption
     * consume - all decrypted blocks are enqueued to be consumed by
       application callbacks
    
    Application handler is registered with POUCH_DOWNLINK_HANDLER(). There
    are 2 application callbacks that are invoked for each pouch entry or
    stream fragment:
     * void (*pouch_downlink_start_cb)(unsigned int stream_id,
                                       const char *path,
                                       uint16_t content_type);
     * void (*pouch_downlink_data_cb)(unsigned int stream_id,
                                      const void *data,
                                      size_t len,
                                      bool is_last);
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- src/downlink.c
+++ src/downlink.c
@@ -3,41 +3,215 @@
  */
 
 #include <stdlib.h>
+#include <zephyr/sys/byteorder.h>
 
-#include <psa/crypto.h>
-
+#include <pouch/downlink.h>
+#include <pouch/types.h>
 #include <pouch/transport/downlink.h>
+#include "cddl/header_decode.h"
+
+#include "block.h"
+#include "crypto.h"
+#include "downlink.h"
+#include "entry.h"
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(downlink, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(downlink);
 
-static psa_hash_operation_t hash;
-uint32_t byte_counter = 0;
+static struct pouch_buf *pouch_buf;
+static bool pouch_header;
+
+static struct
+{
+    pouch_buf_queue_t queue;
+    struct k_work work;
+} decrypt;
+
+static struct
+{
+    pouch_buf_queue_t queue;
+    struct k_work work;
+} consume;
+
+static void decrypt_blocks(struct k_work *work);
+static void consume_blocks(struct k_work *work);
+
+int downlink_init(const struct pouch_config *config)
+{
+    buf_queue_init(&decrypt.queue);
+    k_work_init(&decrypt.work, decrypt_blocks);
+
+    buf_queue_init(&consume.queue);
+    k_work_init(&consume.work, consume_blocks);
+
+    return 0;
+}
+
+static void consume_blocks(struct k_work *work)
+{
+    struct pouch_buf *pouch_buf = buf_queue_get(&consume.queue);
+    if (!pouch_buf)
+    {
+        return;
+    }
+
+    pouch_downlink_block_push(pouch_buf);
+
+    buf_free(pouch_buf);
+
+    if (!buf_queue_is_empty(&consume.queue))
+    {
+        k_work_submit(work);
+    }
+}
+
+static void decrypt_blocks(struct k_work *work)
+{
+    struct pouch_buf *decrypted = crypto_decrypt_block(buf_queue_get(&decrypt.queue));
+    if (!decrypted)
+    {
+        return;
+    }
+
+    buf_queue_submit(&consume.queue, decrypted);
+    k_work_submit(&consume.work);
+
+    if (!buf_queue_is_empty(&decrypt.queue))
+    {
+        k_work_submit(work);
+    }
+}
+
+static void block_downlink_push(struct pouch_buf *pouch_buf)
+{
+    buf_queue_submit(&decrypt.queue, pouch_buf);
+    k_work_submit(&decrypt.work);
+}
 
 void pouch_downlink_start(void)
 {
     LOG_DBG("Pouch downlink start");
 
-    byte_counter = 0;
+    pouch_header = false;
 
-    hash = psa_hash_operation_init();
-    psa_hash_setup(&hash, PSA_ALG_SHA_256);
+    pouch_buf = buf_alloc(CONFIG_POUCH_BLOCK_SIZE);
+    if (!pouch_buf)
+    {
+        LOG_ERR("Failed to allocate pouch buf");
+        return;
+    }
+}
+
+static size_t pouch_downlink_parse_header(struct pouch_bufview *v)
+{
+    const uint8_t *header_raw = pouch_bufview_read(v, 0);
+    struct pouch_header header;
+    size_t header_len;
+    int ret;
+
+    ret = cbor_decode_pouch_header(header_raw, pouch_bufview_available(v), &header, &header_len);
+    if (ret != ZCBOR_SUCCESS)
+    {
+        LOG_DBG("Failed to decode pouch header: %d", ret);
+        return 0;
+    }
+
+    LOG_HEXDUMP_DBG(header_raw, header_len, "pouch header raw");
+
+    LOG_DBG("Header version %d", (int) header.version);
+    LOG_DBG("Payload len %d", (int) header_len);
+
+    return header_len;
 }
 
 void pouch_downlink_push(const void *buf, size_t buf_len)
 {
+    const uint8_t *buf_p = buf;
+
     LOG_HEXDUMP_DBG(buf, buf_len, "Pouch downlink push: ");
 
-    byte_counter += buf_len;
+    while (buf_len)
+    {
+        if (!pouch_buf)
+        {
+            LOG_WRN("No pouch_buf allocated");
+            return;
+        }
 
-    psa_hash_update(&hash, buf, buf_len);
+        if (buf_size_get(pouch_buf) >= CONFIG_POUCH_BLOCK_SIZE)
+        {
+            LOG_ERR("No more space for pouch header");
+            return;
+        }
+
+        size_t buf_written = MIN(buf_len, CONFIG_POUCH_BLOCK_SIZE - buf_size_get(pouch_buf));
+        buf_write(pouch_buf, buf_p, buf_written);
+
+        buf_p += buf_written;
+        buf_len -= buf_written;
+
+        struct pouch_bufview v;
+        pouch_bufview_init(&v, pouch_buf);
+
+        if (!pouch_header)
+        {
+            size_t header_len = pouch_downlink_parse_header(&v);
+
+            if (!header_len)
+            {
+                return;
+            }
+
+            pouch_header = true;
+
+            /* Align first block with pouch_buf start */
+            buf_trim_start(pouch_buf, header_len);
+
+            LOG_HEXDUMP_DBG(pouch_bufview_read(&v, 0), pouch_bufview_available(&v), "remaining");
+        }
+
+        if (pouch_bufview_available(&v) > sizeof(uint16_t))
+        {
+            uint16_t block_size = pouch_bufview_read_be16(&v);
+
+            if (block_size >= CONFIG_POUCH_BLOCK_SIZE)
+            {
+                LOG_ERR("Block size %u is bigger than supported %u",
+                        (unsigned int) block_size,
+                        (unsigned int) CONFIG_POUCH_BLOCK_SIZE);
+                return;
+            }
+
+            if (pouch_bufview_available(&v) >= block_size)
+            {
+                LOG_DBG("Block ready %d available %d",
+                        (int) block_size,
+                        (int) pouch_bufview_available(&v));
+
+                struct pouch_buf *pouch_buf_to_send = pouch_buf;
+
+                pouch_buf = buf_alloc(CONFIG_POUCH_BLOCK_SIZE);
+                if (!pouch_buf)
+                {
+                    LOG_ERR("Failed to allocate pouch buf");
+                }
+
+                if (pouch_buf && pouch_bufview_available(&v) > block_size)
+                {
+                    const uint8_t *remaining = pouch_bufview_read(&v, 0);
+                    remaining += block_size;
+                    size_t remaining_len = pouch_bufview_available(&v) - block_size;
+
+                    LOG_HEXDUMP_DBG(remaining, remaining_len, "remaining");
+
+                    buf_write(pouch_buf, remaining, remaining_len);
+                    buf_trim_end(pouch_buf_to_send, remaining_len);
+                }
+
+                block_downlink_push(pouch_buf_to_send);
+            }
+        }
+    }
 }
 
-void pouch_downlink_finish(void)
-{
-    uint8_t hash_out[32];
-    size_t hash_length;
-    psa_hash_finish(&hash, hash_out, 32, &hash_length);
-    LOG_HEXDUMP_DBG(hash_out, sizeof(hash_out), "Pouch downlink finish: ");
-    LOG_DBG("Total bytes: %d", byte_counter);
-}
+void pouch_downlink_finish(void) {}
