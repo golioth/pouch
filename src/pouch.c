b83873af164074784bcbacd7526c4972fa948de4
Sam Friedman <sam@golioth.io>
2025-07-22 17:33:07 -0400

    events: emit events on pouch work queue
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- src/pouch.c
+++ src/pouch.c
@@ -17,13 +17,28 @@
 K_THREAD_STACK_DEFINE(pouch_stack, CONFIG_POUCH_THREAD_STACK_SIZE);
 
 static struct k_work_q pouch_work_q;
+static struct k_work event_work;
+
+K_MSGQ_DEFINE(pouch_event_q, sizeof(enum pouch_event), CONFIG_POUCH_EVENT_QUEUE_DEPTH, 1);
+
+static void dispatch_events(struct k_work *work)
+{
+    enum pouch_event event;
+
+    while (0 == k_msgq_get(&pouch_event_q, &event, K_NO_WAIT))
+    {
+        STRUCT_SECTION_FOREACH(pouch_event_handler, handler)
+        {
+            handler->callback(event, handler->ctx);
+        }
+    }
+}
 
 void pouch_event_emit(enum pouch_event event)
 {
-    STRUCT_SECTION_FOREACH(pouch_event_handler, handler)
-    {
-        handler->callback(event, handler->ctx);
-    }
+    k_msgq_put(&pouch_event_q, &event, K_NO_WAIT);
+
+    k_work_submit_to_queue(&pouch_work_q, &event_work);
 }
 
 static int pouch_module_init(void)
@@ -35,6 +50,8 @@ static int pouch_module_init(void)
                        K_THREAD_STACK_SIZEOF(pouch_stack),
                        CONFIG_POUCH_THREAD_PRIORITY,
                        NULL);
+
+    k_work_init(&event_work, dispatch_events);
 
     return 0;
 }
