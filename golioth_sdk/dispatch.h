27f56012915f9831f81b1b8bc88cfc8006e04376
Sam Friedman <sam@golioth.io>
2025-07-31 16:47:43 -0400

    golioth: dispatch: separate uplink and downlink
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- golioth_sdk/dispatch.h
+++ golioth_sdk/dispatch.h
@@ -15,28 +15,37 @@ typedef void (*golioth_service_uplink_cb)(void);
 
 #define STREAM_ID_INVALID (-1)
 
-struct golioth_service_data
+struct golioth_downlink_service_data
 {
     int stream_id;
 };
 
-struct golioth_service
+struct golioth_downlink_service
 {
     const char *path;
     golioth_service_downlink_cb downlink_cb;
-    golioth_service_uplink_cb uplink_cb;
-    struct golioth_service_data *data;
+    struct golioth_downlink_service_data *data;
 };
 
-#define GOLIOTH_SERVICE(_name, _path, _downlink_cb, _uplink_cb)                           \
-    BUILD_ASSERT((_path == NULL) == (_downlink_cb == NULL),                               \
-                 "_path and _downlink_cb must either both be valid or both be NULL");     \
-    static struct golioth_service_data _name##_data = {                                   \
+struct golioth_uplink_service
+{
+    golioth_service_uplink_cb uplink_cb;
+};
+
+#define GOLIOTH_DOWNLINK_HANDLER(_name, _path, _downlink_cb)                           \
+    BUILD_ASSERT((_path != NULL) && (_downlink_cb != NULL),                               \
+                 "_path, and _downlink_cb must not be NULL");     \
+    static struct golioth_downlink_service_data _name##_data = {                                   \
         .stream_id = STREAM_ID_INVALID,                                                   \
     };                                                                                    \
-    static STRUCT_SECTION_ITERABLE(golioth_service, CONCAT(_golioth_service_, _name)) = { \
+    static STRUCT_SECTION_ITERABLE(golioth_downlink_service, CONCAT(_golioth_downlink_service_, _name)) = { \
         .path = _path,                                                                    \
         .downlink_cb = _downlink_cb,                                                      \
-        .uplink_cb = _uplink_cb,                                                          \
         .data = &_name##_data,                                                            \
+    }
+
+#define GOLIOTH_UPLINK_HANDLER(_name, _uplink_cb) \
+    BUILD_ASSERT(_uplink_cb != NULL, "_uplink_cb must not be NULL"); \
+    static STRUCT_SECTION_ITERABLE(golioth_uplink_service, CONCAT(_golioth_uplink_service_, _name)) = { \
+        .uplink_cb = _uplink_cb, \
     }
