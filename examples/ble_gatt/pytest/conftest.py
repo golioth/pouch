a21a5d172ed10760a1a5d3fc724e7ef5fce048f5
Mike Szczys <mike@golioth.io>
2026-01-28 11:21:34 -0600

    CI: add dynamically generated certificates
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- examples/ble_gatt/pytest/conftest.py
+++ examples/ble_gatt/pytest/conftest.py
@@ -11,8 +11,6 @@ from pytest_hil.frdmrw612 import FRDMRW612
 
 parser = argparse.ArgumentParser()
 
-SERVER_NAME = "pouch_hil"
-
 
 @pytest.fixture(scope="session")
 def anyio_backend():
@@ -29,6 +27,8 @@ def pytest_addoption(parser):
     parser.addoption("--gw-fw-image", action="store", help="Gateway firmware binary")
     parser.addoption("--gw-serial-number", type=str, action="store", help="Gateway programmer serial number")
 
+def rand_str():
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(16))
 
 class DeviceCredential:
     def __init__(self, name, psk_id, psk):
@@ -36,19 +36,148 @@ class DeviceCredential:
         self.psk_id = psk_id
         self.psk = psk
 
-
 @pytest.fixture(scope="module")
 async def certificate_cred(request, project):
-    # TODO: generate certificate here
-    device_name = 'salmon-itchy-bobcat'
+    device_name = f"ble_{rand_str()}"
+    gateway_name = f"gw_{rand_str()}"
+
+    # Check cloud to verify device does not exist
+
+    with pytest.raises(Exception):
+        device = await project.device_by_name(device_name)
+
+    subprocess.run(
+        [
+            "openssl",
+            "ecparam",
+            "-name",
+            "prime256v1",
+            "-genkey",
+            "-noout",
+            "-out",
+            f"{gateway_name}.key.pem",
+        ],
+        cwd=request.config.rootdir,
+    )
+
+    subprocess.run(
+        [
+            "openssl",
+            "req",
+            "-x509",
+            "-new",
+            "-nodes",
+            "-key",
+            f'{gateway_name}.key.pem',
+            "-sha256",
+            "-subj",
+            f"/CN={gateway_name} CA",
+            "-days",
+            "10",
+            "-out",
+            f'{gateway_name}.crt.pem',
+        ],
+        cwd=request.config.rootdir,
+    )
+
+    # Pass root public key to Golioth server
+
+    with open(f"{gateway_name}.crt.pem", "rb") as f:
+        cert_pem = f.read()
+    root_cert = await project.certificates.add(cert_pem, "root")
+
+    # Device Cert
+
+    subprocess.run(
+        [
+            "openssl",
+            "ecparam",
+            "-name",
+            "prime256v1",
+            "-genkey",
+            "-noout",
+            "-out",
+            f"{device_name}.key.pem",
+        ],
+        cwd=request.config.rootdir,
+    )
+
+    subprocess.run(
+        [
+            "openssl",
+            "req",
+            "-new",
+            "-key",
+            f"{device_name}.key.pem",
+            "-subj",
+            f"/O={project.info['id']}/CN={device_name}",
+            "-out",
+            f"{device_name}.csr.pem",
+        ],
+        cwd=request.config.rootdir,
+    )
+
+    subprocess.run(
+        [
+            "openssl",
+            "x509",
+            "-req",
+            "-in",
+            f"{device_name}.csr.pem",
+            "-CA",
+            f'{gateway_name}.crt.pem',
+            "-CAkey",
+            f'{gateway_name}.key.pem',
+            "-CAcreateserial",
+            "-out",
+            f"{device_name}.crt.pem",
+            "-days",
+            "7",
+            "-sha256",
+        ],
+        cwd=request.config.rootdir,
+    )
+
+    subprocess.run(
+        [
+            "openssl",
+            "ec",
+            "-in",
+            f"{device_name}.key.pem",
+            "-outform",
+            "DER",
+            "-out",
+            f"{device_name}.key.der",
+        ],
+        cwd=request.config.rootdir,
+    )
+
+    subprocess.run(
+        [
+            "openssl",
+            "x509",
+            "-in",
+            f"{device_name}.crt.pem",
+            "-outform",
+            "DER",
+            "-out",
+            f"{device_name}.crt.der",
+        ],
+        cwd=request.config.rootdir,
+    )
 
     yield DeviceCredential(
-        f'{device_name}',
+        f"{device_name}",
         f"{Path(request.config.rootdir, f'{device_name}.key.der')}",
         f"{Path(request.config.rootdir, f'{device_name}.crt.der')}",
     )
 
-    # TODO remove generated certificate here
+    await project.certificates.delete(root_cert["data"]["id"])
+
+    for p in Path(request.config.rootdir).glob(f"{gateway_name}.*"):
+        p.unlink()
+    for p in Path(request.config.rootdir).glob(f"{device_name}.*"):
+        p.unlink()
 
 
 @pytest.fixture(scope="module")
