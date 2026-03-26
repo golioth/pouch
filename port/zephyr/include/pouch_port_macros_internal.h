fd0b7e504fa3e592f8ac4b4bd91832811deca240
Mike Szczys <mike@golioth.io>
2026-03-26 17:39:09 -0500

    port: zephyr: add logging abstraction
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- port/zephyr/include/pouch_port_macros_internal.h
+++ port/zephyr/include/pouch_port_macros_internal.h
@@ -19,3 +19,32 @@
         return 0;                                          \
     }                                                      \
     SYS_INIT(_function##_app_startup_hook, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY)
+
+/*--------------------------------------------------
+ * Logging
+ *------------------------------------------------*/
+
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_ctrl.h>
+
+#define POUCH_LOG_LEVEL_NONE_INTERNAL LOG_LEVEL_NONE
+#define POUCH_LOG_LEVEL_ERR_INTERNAL LOG_LEVEL_ERR
+#define POUCH_LOG_LEVEL_WRN_INTERNAL LOG_LEVEL_WRN
+#define POUCH_LOG_LEVEL_INF_INTERNAL LOG_LEVEL_INF
+#define POUCH_LOG_LEVEL_DBG_INTERNAL LOG_LEVEL_DBG
+#define POUCH_LOG_LEVEL_VERBOSE_INTERNAL LOG_LEVEL_DBG
+
+/* The Zephyr port doesn't use POUCH_LOG_TAG but it needs to be defined for macros to work */
+#define POUCH_LOG_TAG
+
+#define POUCH_LOG_REGISTER_INTERNAL(tag, level) LOG_MODULE_REGISTER(tag, level)
+
+#define POUCH_LOG_ERR_INTERNAL(tag, ...) LOG_ERR(__VA_ARGS__)
+#define POUCH_LOG_WRN_INTERNAL(tag, ...) LOG_WRN(__VA_ARGS__)
+#define POUCH_LOG_INF_INTERNAL(tag, ...) LOG_INF(__VA_ARGS__)
+#define POUCH_LOG_DBG_INTERNAL(tag, ...) LOG_DBG(__VA_ARGS__)
+#define POUCH_LOG_VERBOSE_INTERNAL(tag, ...) LOG_DBG(__VA_ARGS__)
+
+#define POUCH_LOG_HEXDUMP_INTERNAL(tag, buf, size, label) LOG_HEXDUMP_DBG(buf, size, label)
+
+#define POUCH_LOG_FLUSH_INTERNAL() LOG_PANIC()
