57b5d4072b88a3592440967c89e64951b10cb88a
Mike Szczys <mike@golioth.io>
2026-03-27 12:27:00 -0500

    port: esp_idf: add logging to the test application
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- port/esp_idf/test/main/main.c
+++ port/esp_idf/test/main/main.c
@@ -2,6 +2,8 @@
 #include <pouch/port.h>
 #include "other_test_functions.h"
 
+POUCH_LOG_REGISTER(main, POUCH_LOG_LEVEL_DBG);
+
 int _startup_number;
 
 void init_startup_number(void)
@@ -10,9 +12,21 @@ void init_startup_number(void)
 }
 POUCH_APPLICATION_STARTUP_HOOK(init_startup_number);
 
+void test_logging(void)
+{
+    POUCH_LOG_ERR("This is an error: %i", 1);
+    POUCH_LOG_WRN("This is a warning: %i", 2);
+    POUCH_LOG_INF("This is informational: %i", 3);
+    POUCH_LOG_DBG("This is debugging: %i", 4);
+    POUCH_LOG_VERBOSE("This is verbose: %i", 5);
+}
+
 void app_main(void)
 {
-    printf("Hello, World!\n");
-    printf("Startup number: %i\n", _startup_number);
-    printf("Other test number: %i\n", get_other_test_number());
+    POUCH_LOG_INF("Hello, World!");
+    POUCH_LOG_INF("Startup number: %i", _startup_number);
+    POUCH_LOG_INF("Other test number: %i", get_other_test_number());
+
+    test_logging();
+    test_other_logging();
 }
