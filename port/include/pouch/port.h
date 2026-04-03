28a6cabd4216f6d67c0510c742acd7e7f9224bde
Mike Szczys <michael.szczys@canonical.com>
2026-04-03 10:45:34 -0500

    port: misc: add CONTAINER_OF if not available
    Make the CONTAINER_OF macro available on non-Zephyr platforms
    
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/include/pouch/port.h
+++ port/include/pouch/port.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2026 Golioth, Inc.
+ * Copyright (c) 2011-2014, Wind River Systems, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -21,6 +22,54 @@
 #ifndef DIV_ROUND_UP
 #define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
 #endif
+
+#ifndef CONTAINER_OF
+
+/*
+ * When not present, use the Zephyr definition of CONTAINER_OF from:
+ * https://github.com/zephyrproject-rtos/zephyr/blob/v4.3.0/include/zephyr/sys/util.h
+ */
+
+#ifndef __cplusplus
+
+#define SAME_TYPE(a, b) __builtin_types_compatible_p(__typeof__(a), __typeof__(b))
+
+#define CONTAINER_OF_VALIDATE(ptr, type, field)                                            \
+    POUCH_STATIC_ASSERT(SAME_TYPE(*(ptr), ((type *) 0)->field) || SAME_TYPE(*(ptr), void), \
+                        "pointer type mismatch in CONTAINER_OF");
+
+#else
+#define CONTAINER_OF_VALIDATE(ptr, type, field)
+#endif
+
+/**
+ * @brief Get a pointer to a structure containing the element
+ *
+ * Example:
+ *
+ *	struct foo {
+ *		int bar;
+ *	};
+ *
+ *	struct foo my_foo;
+ *	int *ptr = &my_foo.bar;
+ *
+ *	struct foo *container = CONTAINER_OF(ptr, struct foo, bar);
+ *
+ * Above, @p container points at @p my_foo.
+ *
+ * @param ptr pointer to a structure element
+ * @param type name of the type that @p ptr is an element of
+ * @param field the name of the field within the struct @p ptr points to
+ * @return a pointer to the structure that contains @p ptr
+ */
+#define CONTAINER_OF(ptr, type, field)                         \
+    ({                                                         \
+        CONTAINER_OF_VALIDATE(ptr, type, field)                \
+        ((type *) (((char *) (ptr)) - offsetof(type, field))); \
+    })
+
+#endif  // CONTAINER_OF
 
 /*--------------------------------------------------
  * Application Startup Hook
