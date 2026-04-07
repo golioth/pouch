c2b50eeb3848e7e1be416bc663ad1437c42b8f25
Mike Szczys <michael.szczys@canonical.com>
2026-04-07 10:11:19 -0500

    port: freertos: add work queue abstraction
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/freertos/freertos_port_layer.h
+++ port/freertos/freertos_port_layer.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "freertos/FreeRTOS.h"
+#include <pouch/port.h>
 #include <stdint.h>
 
 /*--------------------------------------------------
@@ -19,6 +20,10 @@
 #pragma GCC diagnostic pop
 
 typedef uint32_t pouch_atomic_internal_t;
+
+/* Forward declaration (used in Work Queue section) prevents circular dependency */
+/* This must match what's in port.h */
+typedef pouch_atomic_internal_t pouch_atomic_t;
 
 /*--------------------------------------------------
  * Linked List
@@ -60,3 +65,36 @@ typedef SemaphoreHandle_t pouch_mutex_internal_t;
         configASSERT(name != NULL);                               \
     }                                                             \
     POUCH_APPLICATION_STARTUP_HOOK(static_mutex_init_##name)
+
+/*--------------------------------------------------
+ * Work Queue
+ *------------------------------------------------*/
+
+#define POUCH_FREERTOS_WORK_Q_DEFAULT_QUEUE_LENGTH 10
+#define POUCH_FREERTOS_WORK_FLAG_QUEUED 0
+
+typedef struct pouch_freertos_work_q pouch_work_q_internal_t;
+typedef struct pouch_freertos_work pouch_work_internal_t;
+
+/* Forward declarations to avoid circular dependencies. These must match port.h */
+typedef pouch_work_internal_t pouch_work_t;
+typedef pouch_work_q_internal_t pouch_work_q_t;
+typedef void (*pouch_work_handler_t)(pouch_work_t *work);
+
+struct pouch_freertos_work_q
+{
+    TaskHandle_t handle;
+    StaticTask_t priv_task_buf;
+
+    QueueHandle_t items;
+    StaticQueue_t priv_static_queue;
+    uint8_t priv_storage[POUCH_FREERTOS_WORK_Q_DEFAULT_QUEUE_LENGTH * sizeof(pouch_work_t *)];
+};
+
+struct pouch_freertos_work
+{
+    pouch_atomic_t flags;
+    pouch_work_handler_t handler;
+};
+
+#define POUCH_THREAD_STACK_DEFINE_INTERNAL(name, size) StackType_t name[size / sizeof(StackType_t)]
