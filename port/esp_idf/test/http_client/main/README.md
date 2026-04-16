fceffae186536e3b3a3746fe84a49329bbc2f35b
Mike Szczys <michael.szczys@canonical.com>
2026-04-16 17:10:29 -0500

    esp_idf: http_client: add WiFi support
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/esp_idf/test/http_client/main/README.md
+++ port/esp_idf/test/http_client/main/README.md
@@ -1,3 +1,16 @@
 # Pouch HTTP Client Example (ESP-IDF)
 
 Minimal example to facilitate a build-test for Pouch on ESP-IDF.
+
+## WiFi Provisioning
+
+Open the project configuration menu (`idf.py menuconfig`).
+
+In the `Example: Pouch HTTP Client` menu:
+
+* Set the Wi-Fi configuration.
+    * Set `WiFi SSID`.
+    * Set `WiFi Password`.
+
+Optional: If you need, change the other options according to your
+requirements.
--- port/esp_idf/test/http_client/main/main.c
+++ port/esp_idf/test/http_client/main/main.c
@@ -10,10 +10,28 @@
 #include <pouch/pouch.h>
 
 #include "credentials.h"
+#include "nvs_flash.h"
+#include "wifi.h"
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Pouch HTTP Client Example");
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        nvs_flash_erase();
+        ret = nvs_flash_init();
+    }
+    if (ESP_OK != ret)
+    {
+        ESP_LOGE(TAG, "Failed to initialize NVS: %d", ret);
+        return;
+    }
+
+    ESP_LOGI(TAG, "Initializing WiFi");
+    wifi_init_sta();
 
     struct pouch_config config = {0};
     int err = fill_pouch_config(&config);
