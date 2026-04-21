b28dd268096cadabba5714a5860f12293d069357
Mike Szczys <michael.szczys@canonical.com>
2026-04-21 16:45:04 -0500

    port: esp_idf: http_client: send device cert to server
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/esp_idf/test/http_client/main/credentials.h
+++ port/esp_idf/test/http_client/main/credentials.h
@@ -9,5 +9,6 @@
 #include <pouch/pouch.h>
 #include "mtls_type.h"
 
+int get_device_cert(struct pouch_cert *cert);
 int fill_pouch_config(struct pouch_config *config);
 void fill_mtls_credentials(struct mtls_credentials *creds);
