10e12f0d9c49a2c44866739c82e80306c77d0c16
Mike Szczys <michael.szczys@canonical.com>
2026-04-21 14:27:37 -0500

    port: esp_idf: http_client: add sync_context
    Add sync context and pass it to all functions dealing with the Pouch sync
    process.
    
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/esp_idf/test/http_client/main/http_client.c
+++ port/esp_idf/test/http_client/main/http_client.c
@@ -20,27 +20,36 @@
 #define HTTP_PATH_SERVER_CERT "/.g/server-cert"
 #define HTTP_PATH_DEVICE_CERT "/.g/device-cert"
 
-static struct mtls_credentials *_mtls_creds;
-
 static struct get_server_cert_context
 {
     uint8_t cert_buf[CONFIG_POUCH_HTTP_SERVER_CRT_MAX_SIZE];
     size_t pos;
-} _server_cert_ctx;
+};
+
+static struct sync_context
+{
+    struct mtls_credentials *mtls_creds;
+    struct get_server_cert_context server_cert;
+} _sync_ctx;
+
+static void sync_context_init(struct sync_context *sync, struct mtls_credentials *mtls_creds)
+{
+    sync->mtls_creds = mtls_creds;
+}
 
 static esp_err_t pouch_server_cert_response_callback(esp_http_client_event_t *evt)
 {
     if (NULL == evt->user_data)
     {
-        ESP_LOGE(TAG, "Server cert context is NULL");
+        ESP_LOGE(TAG, "Sync context is NULL");
         return ESP_FAIL;
     }
 
-    struct get_server_cert_context *ctx = (struct get_server_cert_context *) evt->user_data;
+    struct sync_context *sync = (struct sync_context *) evt->user_data;
 
     if (HTTP_EVENT_ON_FINISH == evt->event_id)
     {
-        ESP_LOGI(TAG, "Server cert downloaded: %zu bytes", ctx->pos);
+        ESP_LOGI(TAG, "Server cert downloaded: %zu bytes", sync->server_cert.pos);
         return ESP_OK;
     }
 
@@ -56,14 +65,14 @@ static esp_err_t pouch_server_cert_response_callback(esp_http_client_event_t *ev
     if (!esp_http_client_is_chunked_response(evt->client))
     {
 
-        if (ctx->pos + evt->data_len > sizeof(ctx->cert_buf))
+        if (sync->server_cert.pos + evt->data_len > sizeof(sync->server_cert.cert_buf))
         {
             ESP_LOGE(TAG, "Server cert too large for buffer");
             return ESP_ERR_NO_MEM;
         }
 
-        memcpy(ctx->cert_buf + ctx->pos, evt->data, evt->data_len);
-        ctx->pos += evt->data_len;
+        memcpy(sync->server_cert.cert_buf + sync->server_cert.pos, evt->data, evt->data_len);
+        sync->server_cert.pos += evt->data_len;
         ESP_LOGI(TAG, "This is a chunked response");
         return ESP_OK;
     }
@@ -71,10 +80,9 @@ static esp_err_t pouch_server_cert_response_callback(esp_http_client_event_t *ev
     return ESP_OK;
 }
 
-static int fetch_server_cert(struct mtls_credentials *mtls_creds)
+static int fetch_server_cert(struct sync_context *sync)
 {
-    struct get_server_cert_context *cert_ctx = &_server_cert_ctx;
-    cert_ctx->pos = 0;
+    sync->server_cert.pos = 0;
 
     esp_http_client_config_t config = {
         .path = HTTP_PATH_SERVER_CERT,
@@ -82,13 +90,13 @@ static int fetch_server_cert(struct mtls_credentials *mtls_creds)
         .port = CONFIG_POUCH_HTTP_GW_PORT,
         .transport_type = HTTP_TRANSPORT_OVER_SSL,
         .event_handler = pouch_server_cert_response_callback,
-        .cert_pem = mtls_creds->cert_pem,
-        .cert_len = mtls_creds->cert_len,
-        .client_cert_pem = mtls_creds->client_cert_pem,
-        .client_cert_len = mtls_creds->client_cert_len,
-        .client_key_pem = mtls_creds->client_key_pem,
-        .client_key_len = mtls_creds->client_key_len,
-        .user_data = cert_ctx,
+        .cert_pem = sync->mtls_creds->cert_pem,
+        .cert_len = sync->mtls_creds->cert_len,
+        .client_cert_pem = sync->mtls_creds->client_cert_pem,
+        .client_cert_len = sync->mtls_creds->client_cert_len,
+        .client_key_pem = sync->mtls_creds->client_key_pem,
+        .client_key_len = sync->mtls_creds->client_key_len,
+        .user_data = sync,
     };
 
     esp_http_client_handle_t client = esp_http_client_init(&config);
@@ -101,8 +109,8 @@ static int fetch_server_cert(struct mtls_credentials *mtls_creds)
     esp_http_client_cleanup(client);
 
     struct pouch_cert cert = {
-        .buffer = cert_ctx->cert_buf,
-        .size = cert_ctx->pos,
+        .buffer = sync->server_cert.cert_buf,
+        .size = sync->server_cert.pos,
     };
 
     /* TODO: replace log with call to pouch_server_certificate_set(&cert); */
@@ -113,21 +121,25 @@ static int fetch_server_cert(struct mtls_credentials *mtls_creds)
 
 void http_client_transport_init(struct mtls_credentials *mtls_creds)
 {
-    _mtls_creds = mtls_creds;
+    sync_context_init(&_sync_ctx, mtls_creds);
 }
 
-void http_client_transport_sync(void)
+int http_client_transport_sync(void)
 {
-    if (NULL == _mtls_creds)
+    struct sync_context *sync = &_sync_ctx;
+
+    if (NULL == sync->mtls_creds)
     {
-        ESP_LOGE(TAG, "HTTP Client Transport must be initialized");
-        return;
+        ESP_LOGE(TAG, "mTLS credentials not found; run HTTP transport init.");
+        return -EINVAL;
     }
 
-    int err = fetch_server_cert(_mtls_creds);
+    int err = fetch_server_cert(sync);
     if (0 != err)
     {
         ESP_LOGE(TAG, "Failed to download server certificate: %d", err);
-        return;
+        return err;
     }
+
+    return err;
 }
