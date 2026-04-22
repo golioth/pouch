1f1b3c831c5f9143a98ccc8dda8d0874a367dd5c
Mike Szczys <michael.szczys@canonical.com>
2026-04-22 10:37:36 -0500

    port: esp_idf: http_client: make the http_client reusable
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/esp_idf/test/http_client/main/http_client.c
+++ port/esp_idf/test/http_client/main/http_client.c
@@ -37,12 +37,36 @@ static struct sync_context
     pouch_atomic_t flags;
     struct mtls_credentials *mtls_creds;
     struct get_server_cert_context server_cert;
+
+    esp_http_client_handle_t client;
+    http_event_handle_cb event_cb;
+    char url_buf[CONFIG_POUCH_HTTP_CLIENT_URL_BUF_SIZE];
 } _sync_ctx;
 
 static void sync_context_init(struct sync_context *sync, struct mtls_credentials *mtls_creds)
 {
     pouch_atomic_clear(&sync->flags);
     sync->mtls_creds = mtls_creds;
+    sync->client = NULL;
+}
+
+static int set_url(struct sync_context *sync, const char *path)
+{
+    int expected_len = snprintf(sync->url_buf,
+                                sizeof(sync->url_buf),
+                                "https://%s:%d%s",
+                                CONFIG_POUCH_HTTP_GW_URI,
+                                CONFIG_POUCH_HTTP_GW_PORT,
+                                path);
+
+    if (expected_len >= sizeof(sync->url_buf))
+    {
+        /* Adjust CONFIG_POUCH_HTTP_CLIENT_URL_BUF_SIZE */
+        ESP_LOGE(TAG, "URL too long for buffer");
+        return -ENOMEM;
+    }
+
+    return 0;
 }
 
 static esp_err_t pouch_server_cert_response_callback(esp_http_client_event_t *evt)
@@ -96,34 +120,25 @@ static int fetch_server_cert(struct sync_context *sync)
     }
 
     sync->server_cert.pos = 0;
+    sync->event_cb = pouch_server_cert_response_callback;
 
-    esp_http_client_config_t config = {
-        .path = HTTP_PATH_SERVER_CERT,
-        .host = CONFIG_POUCH_HTTP_GW_URI,
-        .port = CONFIG_POUCH_HTTP_GW_PORT,
-        .transport_type = HTTP_TRANSPORT_OVER_SSL,
-        .event_handler = pouch_server_cert_response_callback,
-        .cert_pem = sync->mtls_creds->cert_pem,
-        .cert_len = sync->mtls_creds->cert_len,
-        .client_cert_pem = sync->mtls_creds->client_cert_pem,
-        .client_cert_len = sync->mtls_creds->client_cert_len,
-        .client_key_pem = sync->mtls_creds->client_key_pem,
-        .client_key_len = sync->mtls_creds->client_key_len,
-        .user_data = sync,
-    };
+    int err = set_url(sync, HTTP_PATH_SERVER_CERT);
+    if (0 != err)
+    {
+        return err;
+    }
 
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_err_t err = esp_http_client_perform(client);
+    esp_http_client_set_url(sync->client, (char *) sync->url_buf);
+    esp_http_client_set_method(sync->client, HTTP_METHOD_GET);
+
+    err = esp_http_client_perform(sync->client);
 
     if (err != ESP_OK)
     {
         ESP_LOGE(TAG, "Error performing http request %s", esp_err_to_name(err));
-        err = -(esp_http_client_get_errno(client));
-        esp_http_client_cleanup(client);
+        err = -(esp_http_client_get_errno(sync->client));
         return err;
     }
-
-    esp_http_client_cleanup(client);
 
     struct pouch_cert cert = {
         .buffer = sync->server_cert.cert_buf,
@@ -146,6 +161,40 @@ void http_client_transport_init(struct mtls_credentials *mtls_creds)
     sync_context_init(&_sync_ctx, mtls_creds);
 }
 
+esp_err_t event_handler_proxy(esp_http_client_event_t *evt)
+{
+    if (NULL == evt->user_data)
+    {
+        ESP_LOGE(TAG, "Sync context cannot be NULL");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    struct sync_context *sync = (struct sync_context *) evt->user_data;
+    return sync->event_cb(evt);
+}
+
+esp_http_client_handle_t client_initialize(struct sync_context *sync)
+{
+    esp_http_client_config_t config = {
+        .path = HTTP_PATH_POUCH,
+        .host = CONFIG_POUCH_HTTP_GW_URI,
+        .port = CONFIG_POUCH_HTTP_GW_PORT,
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,
+        .event_handler = event_handler_proxy,
+        .cert_pem = sync->mtls_creds->cert_pem,
+        .cert_len = sync->mtls_creds->cert_len,
+        .client_cert_pem = sync->mtls_creds->client_cert_pem,
+        .client_cert_len = sync->mtls_creds->client_cert_len,
+        .client_key_pem = sync->mtls_creds->client_key_pem,
+        .client_key_len = sync->mtls_creds->client_key_len,
+        .timeout_ms = HTTP_TIMEOUT_MS,
+        .user_data = sync,
+    };
+
+    ESP_LOGI(TAG, "Initializing http client");
+    return esp_http_client_init(&config);
+}
+
 int http_client_transport_sync(void)
 {
     struct sync_context *sync = &_sync_ctx;
@@ -162,6 +211,17 @@ int http_client_transport_sync(void)
         return -EACCES;
     }
 
+    if (NULL == sync->client)
+    {
+        sync->client = client_initialize(sync);
+        if (NULL == sync->client)
+        {
+            ESP_LOGE(TAG, "Failed to initialize http client");
+            pouch_atomic_clear_bit(&sync->flags, IN_USE_FLAG);
+            return -ENOENT;
+        }
+    }
+
     int err = fetch_server_cert(sync);
     if (0 != err)
     {
@@ -171,5 +231,12 @@ int http_client_transport_sync(void)
 
 clear_and_return:
     pouch_atomic_clear_bit(&sync->flags, IN_USE_FLAG);
+
+    if (0 != err)
+    {
+        ESP_LOGI(TAG, "Freeing http client");
+        esp_http_client_cleanup(sync->client);
+        sync->client = NULL;
+    }
     return err;
 }
