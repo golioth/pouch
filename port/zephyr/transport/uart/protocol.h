e9932f7b05fbe1eff818073d49568a1a7b52f7a1
Trond Snekvik <trond.snekvik@canonical.com>
2026-05-15 10:46:44 +0200

    Add serial API

--- port/zephyr/transport/uart/protocol.h
+++ port/zephyr/transport/uart/protocol.h
@@ -4,3 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
+#include <stdlib.h>
+#include <stdbool.h>
+
+enum serial_channel
+{
+    SERIAL_CH_ACK,
+    SERIAL_CH_SERVER_CERT,
+    SERIAL_CH_DEVICE_CERT,
+    SERIAL_CH_UPLINK,
+    SERIAL_CH_DOWNLINK,
+    SERIAL_CH_INFO,
+};
+
+typedef int (*serial_recv_t)(const void *buf, size_t len);
+
+int serial_init(enum serial_channel ch, serial_recv_t callback);
+int serial_send(enum serial_channel ch, const void *data, size_t len);
+int serial_ack(bool success);
