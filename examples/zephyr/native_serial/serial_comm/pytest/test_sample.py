d596fddd350ed142fc9b3ad42958f791e2ee061a
Marcin Niestroj <marcin.niestroj@canonical.com>
2026-06-11 13:25:03 +0200

    examples: zephyr: serial_comm: new sample
    Signed-off-by: Marcin Niestroj <marcin.niestroj@canonical.com>

---
+++ examples/zephyr/native_serial/serial_comm/pytest/test_sample.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2026 Golioth, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+def test_serial_communication(dut, client_process):
+    """Verify that the client can send data to the server over a serial link."""
+    dut.readlines_until("RECEIVED: Hello from client!", timeout=10)
