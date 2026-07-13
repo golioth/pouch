0cf4d7936f2fe8c473cf37bfe9e7478357da0f22
Mike Szczys <michael.szczys@canonical.com>
2026-07-13 15:52:28 -0500

    port: use recursive mutex for FreeRTOS
    use the recursive type of mutex from FreeRTOS. This doesn't change any
    current behavior for the ESP-IDF examples because they only use plain
    lock/unlock.
    
    But an upcoming change in the Zephyr port gatt broker requires a recursive
    mutex to deal with a re-entrant call chain (natively provided by k_mutex).
    We want to ensure the same behavior is available when we implement a gatt
    broker in ESP-IDF.
    
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- port/freertos/freertos_port_layer.h
+++ port/freertos/freertos_port_layer.h
@@ -55,15 +55,15 @@ typedef struct freertos_msgq_ctx pouch_msgq_internal_t;
 
 typedef SemaphoreHandle_t pouch_mutex_internal_t;
 
-#define POUCH_MUTEX_DEFINE_INTERNAL(name)                         \
-    SemaphoreHandle_t name = NULL;                                \
-    StaticSemaphore_t xMutexBuffer_##name;                        \
-                                                                  \
-    static void static_mutex_init_##name(void)                    \
-    {                                                             \
-        name = xSemaphoreCreateMutexStatic(&xMutexBuffer_##name); \
-        configASSERT(name != NULL);                               \
-    }                                                             \
+#define POUCH_MUTEX_DEFINE_INTERNAL(name)                                  \
+    SemaphoreHandle_t name = NULL;                                         \
+    StaticSemaphore_t xMutexBuffer_##name;                                 \
+                                                                           \
+    static void static_mutex_init_##name(void)                             \
+    {                                                                      \
+        name = xSemaphoreCreateRecursiveMutexStatic(&xMutexBuffer_##name); \
+        configASSERT(name != NULL);                                        \
+    }                                                                      \
     POUCH_APPLICATION_STARTUP_HOOK(static_mutex_init_##name)
 
 /*--------------------------------------------------
