5b21a9593bd8e383b3b84704f441ffa061c3094a
Mike Szczys <michael.szczys@canonical.com>
2026-07-20 10:47:49 -0500

    CI: add ota_harness and gateway_provision plugins to ESP-IDF ble_gatt
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- examples/esp_idf/ble_gatt/pytest/conftest.py
+++ examples/esp_idf/ble_gatt/pytest/conftest.py
@@ -13,7 +13,12 @@ sys.path.insert(
     0, str(Path(__file__).resolve().parents[4] / "scripts" / "pytest-pouch")
 )
 
-pytest_plugins = ["pytest_pouch.plugin", "pytest_pouch.esp_idf_harness"]
+pytest_plugins = [
+    "pytest_pouch.plugin",
+    "pytest_pouch.esp_idf_harness",
+    "pytest_pouch.ota_harness",
+    "pytest_pouch.gateway_provision",
+]
 
 
 @pytest.fixture(scope="module")
